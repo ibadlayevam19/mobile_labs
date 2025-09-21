2c90c756c120735d3d25a6889737a205a38c05a2
ibadlayevam19 <malika19102005@gmail.com>
2025-09-21 10:27:21 +0500

    my own, not gpt

--- lab4/inheritance/main.dart
+++ lab4/inheritance/main.dart
@@ -1,120 +1,118 @@
-//Task 2
-class Animal {
-  void makeSound() {
-    print("Some animal sound");
+//Task1
+class Vehicle{
+  String make;
+  String model;
+  Vehicle(this.make, this.model);
+  void display(){
+    print("Vehicle: $make $model");
   }
 }
-
-class Dog extends Animal {
+class Car extends Vehicle{
+  int numberOfDoors;
+  Car(String make, String model, this.numberOfDoors): super(make, model);
   @override
-  void makeSound() {
-    print("Woof!");
+  void display(){
+    super.display();
+    print('Doors: $numberOfDoors');
   }
 }
 
-class Cat extends Animal {
+//Task2
+class Animal{
+  void makeSound(){
+    print('Some animal sound');
+  }
+}
+class Dog extends Animal{
   @override
-  void makeSound() {
-    print("Meow!");
+  void makeSound(){
+    print('Woof!');
+  }
+}
+class Cat extends Animal{
+  @override
+  void makeSound(){
+    print('Meow!');
   }
 }
 
-//Task 3
-class Shape {
+//Task3
+class Shape{
   String color;
   Shape(this.color);
 }
-
-class Circle extends Shape {
+class Circle extends Shape{
   double radius;
-  Circle(String color, this.radius) : super(color);
+  Circle(this.radius, String color): super(color);
 }
-
-class Square extends Shape {
+class Square extends Shape{
   double sideLength;
-  Square(String color, this.sideLength) : super(color);
+  Square(this.sideLength, String color): super(color);
 }
 
-//Task 4
-class Employee {
+//Task4
+class Employee{
   String name;
   double baseSalary;
-
   Employee(this.name, this.baseSalary);
-
-  double calculateSalary() {
+  double calculateSalary(){
     return baseSalary;
   }
 }
-
-class Manager extends Employee {
+class Manager extends Employee{
   double bonus;
-
-  Manager(String name, double baseSalary, this.bonus)
-      : super(name, baseSalary);
-
+  Manager(String name, double baseSalary, this.bonus):super(name, baseSalary);
   @override
-  double calculateSalary() {
-    return baseSalary + bonus;
+  double calculateSalary(){
+    return baseSalary+bonus;
   }
 }
 
-//Task 5
-class LibraryItem {
+//Task5
+class LibraryItem{
   String title;
-  int id;
-
+  String id;
   LibraryItem(this.title, this.id);
-
-  String getDetails() {
+  String getDetails(){
     return "Title: $title, ID: $id";
   }
 }
-
-class Book extends LibraryItem {
+class Book extends LibraryItem{
   String author;
-
-  Book(String title, int id, this.author) : super(title, id);
-
+  Book(String id, String title, this.author):super(title, id);
   @override
-  String getDetails() {
+  String getDetails(){
     return "Book - Title: $title, ID: $id, Author: $author";
   }
 }
-
-class DVD extends LibraryItem {
+class DVD extends LibraryItem{
   String director;
-
-  DVD(String title, int id, this.director) : super(title, id);
-
+  DVD( id,  title, this.director):super(id, title);
   @override
-  String getDetails() {
+  String getDetails(){
     return "DVD - Title: $title, ID: $id, Director: $director";
   }
 }
+void main(){
+  //task1
+  var myCar=Car('Honda', 'Civis', 4);
+  myCar.display();
 
-void main() {
-  //Task2 
-  Dog dog = Dog();
-  Cat cat = Cat();
-  dog.makeSound();
+  //task2
+  var cat=Cat();
   cat.makeSound();
 
-  //Task3
-  Circle circle = Circle("Red", 5.0);
-  Square square = Square("Blue", 4.0);
-  print("Circle color: ${circle.color}, radius: ${circle.radius}");
-  print("Square color: ${square.color}, side length: ${square.sideLength}");
+  //task3
+  var circle=Circle(13.2, 'red');
+  print('circle with color ${circle.color} and radius ${circle.radius}');
 
-  //Task4
-  Employee emp = Employee("Alice", 3000);
-  Manager mgr = Manager("Bob", 4000, 1500);
-  print("${emp.name}'s salary: ${emp.calculateSalary()}");
-  print("${mgr.name}'s salary: ${mgr.calculateSalary()}");
+  //task4
+  var employee=Employee('Mike', 827.5);
+  print(employee.calculateSalary());
+  var manager=Manager('Malika', 827, 500);
+  print(manager.calculateSalary());
 
-  //Task5
-  Book book = Book("The Alchemist", 1, "Paulo Coelho");
-  DVD dvd = DVD("Inception", 2, "Christopher Nolan");
+  //task5
+  var book=Book('182', 'Hell yeah', 'Derek');
   print(book.getDetails());
-  print(dvd.getDetails());
 }
--- lab4/mixins/main.dart
+++ lab4/mixins/main.dart
@@ -1,84 +1,100 @@
-//Task 2
-mixin Flyer {
-  void fly() {
-    print("Taking off!");
+//Task1
+mixin Swimmer{
+  void swim(){
+    print("Moving through the water...");
+  }
+}
+class Human with Swimmer{
+  String name;
+  Human(this.name);
+}
+class Fish with Swimmer{
+  String species;
+  Fish(this.species);
+}
+
+//Task2
+mixin Flyer{
+  void fly(){
+    print('Taking off!');
+  }
+}
+class Bird with Flyer{}
+class Plane with Flyer{}
+
+//Task3
+mixin Logger{
+  void log(String message){
+    print('[LOG] $message');
+  }
+}
+class DataService with Logger{
+  void fetchData(){
+    log('Fetching data');
   }
 }
 
-class Bird with Flyer {}
-
-class Plane with Flyer {}
-
-//Task 3
-mixin Logger {
-  void log(String message) {
-    print("[LOG] $message");
+//Task4
+mixin Validator{
+  bool isEmailValid(String email){
+    return email.contains('@')&&email.contains('.');
+  }
+  bool isPasswordStrong(String password){
+    return password.length>8;
+  }
+}
+class UserSignup with Validator{
+  void validate(String email, String password){
+    print('Email valid: ${isEmailValid(email)}');
+    print('Password strong: ${isPasswordStrong(password)}');
   }
 }
 
-class DataService with Logger {
-  void fetchData() {
-    log("Fetching data");
+//Task5
+mixin Performer{
+  void perform(){
+    print('Performs.');
   }
 }
-
-//Task 4
-mixin Validator {
-  bool isEmailValid(String email) {
-    return email.contains('@') && email.contains('.');
-  }
-
-  bool isPasswordStrong(String password) {
-    return password.length > 8;
+mixin Musician{
+  void playInstrument(){
+    print('Plays instrument.');
   }
 }
-
-class UserSignup with Validator {
-  void validate(String email, String password) {
-    print("Email valid: ${isEmailValid(email)}");
-    print("Password strong: ${isPasswordStrong(password)}");
+class Artist{
+  void calm(){
+    print('Calms down!');
   }
 }
+class Dancer extends Artist with Performer{}
+class Guitarist extends Artist with Performer, Musician{}
+void main(){
+  //Task1
+  var person=Human('Alice');
+  var fish=Fish('Tuna');
+  person.swim();
+  fish.swim();
 
-//Task 5
-mixin Performer {
-  void perform() {
-    print("Performing on stage!");
-  }
-}
-
-mixin Musician {
-  void playInstrument() {
-    print("Playing an instrument!");
-  }
-}
-
-class Artist {}
-
-class Dancer extends Artist with Performer {}
-
-class Guitarist extends Artist with Performer, Musician {}
-
-void main() {
   //Task2
-  Bird bird = Bird();
-  Plane plane = Plane();
+  Bird bird=Bird();
+  var plane=Plane();
   bird.fly();
   plane.fly();
 
   //Task3
-  DataService service = DataService();
+  DataService service=DataService();
   service.fetchData();
 
   //Task4
-  UserSignup signup = UserSignup();
-  signup.validate("malika@example.com", "mypassword123");
+  UserSignup user=UserSignup();
+  user.validate('m.ibadlayeva@newuu.uz', '123345');
 
   //Task5
-  Dancer dancer = Dancer();
+  Dancer dancer=Dancer();
   dancer.perform();
-
-  Guitarist guitarist = Guitarist();
+  dancer.calm();
+  Guitarist guitarist=Guitarist();
+  guitarist.calm();
   guitarist.perform();
   guitarist.playInstrument();
 }
